e87a0a33e38a30e669e953871fb9f4937babc05c
Scott-cityboy <scottimbiaka@gmail.com>
2023-12-31 01:59:28 +0300

    0x04. AirBnB cloneWeb framework

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -3,8 +3,11 @@
 starts a Flask web application
 """
 
+
 from flask import Flask
 app = Flask(__name__)
+
+
 
 
 @app.route('/', strict_slashes=False)
@@ -13,10 +16,14 @@ def index():
     return 'Hello HBNB!'
 
 
+
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """returns HBNB"""
     return 'HBNB'
+
+
 
 
 @app.route('/c/<text>', strict_slashes=False)
@@ -25,11 +32,22 @@ def cisfun(text):
     return 'C ' + text.replace('_', ' ')
 
 
+
+
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def pythoniscool(text='is cool'):
     """display “Python ”, followed by the value of the text variable"""
     return 'Python ' + text.replace('_', ' ')
+
+
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def imanumber(n):
+    """display “n is a number” only if n is an integer"""
+    return "{:d} is a number".format(n)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
